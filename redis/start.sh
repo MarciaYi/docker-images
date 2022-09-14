911fe1f8beb259c0cecc1ea44d1579f7afbbca36
bytefucker <yihongzhi@cestc.cn>
2022-09-14 17:26:15 +0800

    redis

---
+++ redis/start.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+if [ $REDIS_PASSWORD ]; then 
+  echo "requirepass $REDIS_PASSWORD" >> redis.conf
+fi
+
+if [ $MAX_MEMORY ]; then 
+  echo "maxmemory $MAX_MEMORY" >> redis.conf
+fi
+
+echo "----start redis server----"
+
+cat redis.conf
+
+redis-server redis.conf
